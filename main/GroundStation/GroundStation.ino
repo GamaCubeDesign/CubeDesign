c371cf31dfbdcfbb49f35460bf2d29987b7ea946
VitinLima <aguirra.vitor@gmail.com>
2024-02-27 23:45:27 -0300

    More changes

--- main/GroundStation/GroundStation.ino
+++ main/GroundStation/GroundStation.ino
@@ -23,44 +23,6 @@ void setup(){
   Serial.begin(57600);                   // initialize serial
   while (!Serial);
 
-  satPacket.data.healthData.accel_x = 10.0;
-  satPacket.data.healthData.accel_y = 11.0;
-  satPacket.data.healthData.giros_x = 12.0;
-  satPacket.data.healthData.giros_y = 13.0;
-  Serial.print("CONTROL:STATUS PACKET:");
-  Serial.print(satPacket.data.healthData.time);Serial.print(":");
-  Serial.print(satPacket.data.healthData.index);Serial.print(":");
-  Serial.print(satPacket.data.healthData.sd_memory_usage);Serial.print(":");
-  Serial.print(satPacket.data.healthData.internal_temperature);Serial.print(":");
-  Serial.print(satPacket.data.healthData.external_temperature);Serial.print(":");
-  Serial.print(satPacket.data.healthData.battery_voltage);Serial.print(":");
-  // Serial.print(satPacket.data.healthData.battery_charge);Serial.print(":");
-  // Serial.print(satPacket.data.healthData.battery_current);Serial.print(":");
-  // Serial.print(satPacket.data.healthData.battery_temperature);Serial.print(":");
-  Serial.print(satPacket.data.healthData.altitude);Serial.print(":");
-  Serial.print(satPacket.data.healthData.pressure);Serial.print(":");
-  // Serial.print(satPacket.data.healthData);Serial.print(":");
-  Serial.print(satPacket.data.healthData.accel_x);Serial.print(":");
-  Serial.print(satPacket.data.healthData.accel_y);Serial.print(":");
-  Serial.print(satPacket.data.healthData.giros_x);Serial.print(":");
-  Serial.println(satPacket.data.healthData.giros_y);
-
-  satPacket.data.imagingData.time = 0;
-  satPacket.data.imagingData.index = 0;
-  satPacket.data.imagingData.type = 0;
-  satPacket.data.imagingData.duration = 0;
-  satPacket.data.imagingData.radius = 0;
-  satPacket.data.imagingData.x = 0;
-  satPacket.data.imagingData.y = 0;
-  Serial.print("CONTROL:IMAGING PACKET:");
-  Serial.print(satPacket.data.imagingData.time);Serial.print(":");
-  Serial.print(satPacket.data.imagingData.index);Serial.print(":");
-  Serial.print(satPacket.data.imagingData.type);Serial.print(":");
-  Serial.print(satPacket.data.imagingData.duration);Serial.print(":");
-  Serial.print(satPacket.data.imagingData.radius);Serial.print(":");
-  Serial.print(satPacket.data.imagingData.x);Serial.print(":");
-  Serial.println(satPacket.data.imagingData.y);
-
   Serial.println("PRINT:Gama Ground Station communication system with LoRa Ra-01 rf module");
 
   // override the default CS, reset, and IRQ pins (optional)
@@ -89,6 +51,46 @@ void setup(){
   delay(300);
   digitalWrite(GSCOM_LED, LOW);
   delay(300);
+
+  satPacket.data.healthData.index = 0;
+  satPacket.data.healthData.time = 0;
+  satPacket.data.healthData = 0;
+  satPacket.data.healthData.accel_x = 10.0;
+  satPacket.data.healthData.accel_y = 11.0;
+  satPacket.data.healthData.giros_x = 12.0;
+  satPacket.data.healthData.giros_y = 13.0;
+  Serial.print("CONTROL:STATUS PACKET:");
+  Serial.print(satPacket.data.healthData.time);Serial.print(":");
+  Serial.print(satPacket.data.healthData.index);Serial.print(":");
+  Serial.print(satPacket.data.healthData.sd_memory_usage);Serial.print(":");
+  Serial.print(satPacket.data.healthData.internal_temperature);Serial.print(":");
+  Serial.print(satPacket.data.healthData.external_temperature);Serial.print(":");
+  Serial.print(satPacket.data.healthData.battery_voltage);Serial.print(":");
+  // Serial.print(satPacket.data.healthData.battery_charge);Serial.print(":");
+  // Serial.print(satPacket.data.healthData.battery_current);Serial.print(":");
+  // Serial.print(satPacket.data.healthData.battery_temperature);Serial.print(":");
+  Serial.print(satPacket.data.healthData.altitude);Serial.print(":");
+  Serial.print(satPacket.data.healthData.pressure);Serial.print(":");
+  Serial.print(satPacket.data.healthData.accel_x);Serial.print(":");
+  Serial.print(satPacket.data.healthData.accel_y);Serial.print(":");
+  Serial.print(satPacket.data.healthData.giros_x);Serial.print(":");
+  Serial.println(satPacket.data.healthData.giros_y);
+
+  satPacket.data.imagingData.time = 0;
+  satPacket.data.imagingData.index = 0;
+  satPacket.data.imagingData.type = 0;
+  satPacket.data.imagingData.duration = 0;
+  satPacket.data.imagingData.radius = 0;
+  satPacket.data.imagingData.x = 0;
+  satPacket.data.imagingData.y = 0;
+  Serial.print("CONTROL:IMAGING PACKET:");
+  Serial.print(satPacket.data.imagingData.time);Serial.print(":");
+  Serial.print(satPacket.data.imagingData.index);Serial.print(":");
+  Serial.print(satPacket.data.imagingData.type);Serial.print(":");
+  Serial.print(satPacket.data.imagingData.duration);Serial.print(":");
+  Serial.print(satPacket.data.imagingData.radius);Serial.print(":");
+  Serial.print(satPacket.data.imagingData.x);Serial.print(":");
+  Serial.println(satPacket.data.imagingData.y);
 }
 
 bool state_sending = false;
